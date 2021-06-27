249f5a5dc319dc7d0b222e232e422d69aaeecbac
Sochima Ezema <sochima@stanford.edu>
2021-06-27 21:48:12 +0000

    Add Cloud Storage java library

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -1,5 +1,10 @@
 package com.google.sps.servlets;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
 import java.util.ArrayList;
 import com.google.gson.Gson;
 import java.io.IOException;
@@ -15,6 +20,15 @@ public class HelloWorldServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     
+    // Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    //     KeyFactory keyFactory = datastore.newKeyFactory().setKind("Visitor");
+    //     FullEntity taskEntity =
+    //         Entity.newBuilder(keyFactory.newKey())
+    //             .set("title", title)
+    //             .set("timestamp", timestamp)
+    //             .build();
+    //     datastore.put(taskEntity);
+
     ArrayList<String> facts = new ArrayList<String>();
     facts.add("I love fashion");
     facts.add("I aspire to be a fshion designer");
